650d86fcb76bb597c4246fb952c3443e7139e120
Sofia19c <sofivampaireknight@gmail.com>
2022-11-27 19:50:59 -0500

    Comentarios Javadoc de la clase Infoincompleta

--- src/main/java/excepciones/InfoIncompleta.java
+++ src/main/java/excepciones/InfoIncompleta.java
@@ -1,12 +1,24 @@
 package excepciones;
 
+/**
+ * Es un tipo de excepcion que ocurre cuando la informacion de un formulario esta incompleta 
+ */
 public class InfoIncompleta extends Exception {
     
     String detalle;
 
+    /**
+     * Construye la excepción 
+     * @param s Texto de la excepcion
+     */
     public InfoIncompleta(String s) {
         detalle=s;
     }
+
+    /**
+     * Este metodo convierte la excepcion a un string
+     * @return Una cadena de texto que contiene la información de la excepción
+     */
 
     public String toString(){
         return "Excepcion de información: " + detalle;
